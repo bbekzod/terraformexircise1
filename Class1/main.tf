ac86965d931bf223f36a82c48a93208ad96a621c
EC2 Default User <ec2-user@ip-172-31-22-62.us-west-2.compute.internal>
2023-03-17 20:01:23 +0000

    Update file

--- Class1/main.tf
+++ Class1/main.tf
@@ -1,3 +1,43 @@
 resource "aws_iam_user" "firstuser" {
-  name = "firstuser"
+  for_each = toset([
+    "firstuser",
+    "seconduser",
+    "thirduser"
+  ])
+  name = each.key
+}
+
+resource "aws_iam_group" "multigroup" {
+  for_each = toset([
+    "sales",
+    "marketing",
+    "develop",
+  ])
+  name = each.key
+}
+
+resource "aws_key_pair" "first" {
+  key_name   = "first-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket_prefix = "my-"
+}
+
+resource "aws_iam_user" "tim" {
+  name = "tim"
+}
+
+
+resource "aws_iam_group" "devops" {
+  name = "devops"
+}
+
+resource "aws_iam_group_membership" "team" {
+  name = "tf-testing-group-membership"
+  users = [
+    aws_iam_user.tim.name
+  ]
+  group = aws_iam_group.devops.name
 }
